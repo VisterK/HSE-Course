a0e3840c2b1018d94783549ed9a6fe98dc10d639
VisterK <vister.k30@gmail.com>
2020-10-30 13:15:54 +0300

    fixed multiplication

--- module-1/homework/BigInteger/biginteger.h
+++ module-1/homework/BigInteger/biginteger.h
@@ -4,13 +4,10 @@
 #include <ostream>
 #include <istream>
 
-#ifndef BIGINTEGER_BIGINTEGER_H
-#define BIGINTEGER_BIGINTEGER_H
+#ifndef BIGINTEGER_H
+#define BIGINTEGER_H
 
 class BigInteger {
-  int length;
-  bool positive;
-  std::vector<int> number;
 public:
   BigInteger(std::string s);
   BigInteger();
@@ -26,35 +23,8 @@ public:
   BigInteger operator++(int);
   BigInteger operator--(int);
 
-  friend std::ostream& operator<<(std::ostream &os, const BigInteger& num) {
-    if(!num.positive){
-      os << "-";
-    }
-    for(int digit = 0; digit < num.length; ++digit){
-      os << num.number[num.length - digit - 1];
-    }
-    return os;
-  }
-  friend std::istream& operator>>(std::istream &is, BigInteger& num) {
-    std::string str;
-    is >> str;
-    num.number.clear();
-    if(str[0] == '-'){
-      num.positive = false;
-      num.length = (int)str.size() - 1;
-      for(int digit = 0; digit < num.length; digit++){
-        num.number.push_back(str[num.length - digit] - '0');
-      }
-    }
-    else{
-      num.positive = true;
-      num.length = (int)str.size();
-      for(int digit = 0; digit < num.length; digit++){
-        num.number.push_back(str[num.length - digit - 1] - '0');
-      }
-    }
-    return is;
-  }
+  friend std::ostream& operator<<(std::ostream &os, const BigInteger& num);
+  friend std::istream& operator>>(std::istream &is, BigInteger& num);
 
   friend bool operator== (const BigInteger& left, const BigInteger& right);
   friend bool operator!= (const BigInteger& left, const BigInteger& right);
@@ -79,8 +49,12 @@ public:
 
   static void extend(std::vector<int>&a, int length);
   static std::vector<int> NaiveMultiplication(std::vector<int>& left, std::vector<int>& right);
-  static std::vector<int> KaratsubaMultiplication(std::vector<int>& x, std::vector<int>& y);
+  static std::vector<int> KaratsubaMultiplication(std::vector<int>&left, std::vector<int>& y);
 
+private:
+  int length;
+  bool positive;
+  std::vector<int> number;
 };
 
-#endif BIGINTEGER_BIGINTEGER_H
+#endif
