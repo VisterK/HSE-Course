c5233a017c48db181c960c1bde22a05e33b8aa4b
VisterK <vister.k30@gmail.com>
2020-10-30 14:22:23 +0300

    fixed line indents

--- module-1/homework/BigInteger/biginteger.h
+++ module-1/homework/BigInteger/biginteger.h
@@ -1,60 +1,59 @@
 #include <iostream>
-#include <vector>
-#include <string>
-#include <ostream>
 #include <istream>
+#include <ostream>
+#include <string>
+#include <vector>
 
 #ifndef BIGINTEGER_H
 #define BIGINTEGER_H
 
 class BigInteger {
 public:
-  BigInteger(std::string s);
-  BigInteger();
-  std::string toString();
-  BigInteger(int x);
-  BigInteger(std::vector<int> &a, bool sign);
+    BigInteger(std::string s);
+    BigInteger();
+    std::string toString();
+    BigInteger(int x);
+    BigInteger(std::vector<int> &a, bool sign);
 
-  BigInteger operator- ();
+    BigInteger operator-();
 
-  BigInteger& operator++();
-  BigInteger& operator--();
+    BigInteger &operator++();
+    BigInteger &operator--();
+    BigInteger operator++(int);
+    BigInteger operator--(int);
 
-  BigInteger operator++(int);
-  BigInteger operator--(int);
+    friend std::ostream &operator<<(std::ostream &os, const BigInteger &num);
+    friend std::istream &operator>>(std::istream &is, BigInteger &num);
 
-  friend std::ostream& operator<<(std::ostream &os, const BigInteger& num);
-  friend std::istream& operator>>(std::istream &is, BigInteger& num);
+    friend bool operator==(const BigInteger &left, const BigInteger &right);
+    friend bool operator!=(const BigInteger &left, const BigInteger &right);
+    friend bool operator<(const BigInteger &left, const BigInteger &right);
+    friend bool operator>(const BigInteger &left, const BigInteger &right);
+    friend bool operator<=(const BigInteger &left, const BigInteger &right);
+    friend bool operator>=(const BigInteger &left, const BigInteger &right);
 
-  friend bool operator== (const BigInteger& left, const BigInteger& right);
-  friend bool operator!= (const BigInteger& left, const BigInteger& right);
-  friend bool operator< (const BigInteger& left, const BigInteger& right);
-  friend bool operator> (const BigInteger& left, const BigInteger& right);
-  friend bool operator<= (const BigInteger& left, const BigInteger& right);
-  friend bool operator>= (const BigInteger& left, const BigInteger& right);
+    operator bool();
 
-  operator bool();
+    friend BigInteger operator+(const BigInteger &l, const BigInteger &r);
+    friend BigInteger operator-(const BigInteger &l, const BigInteger &r);
+    friend BigInteger operator*(const BigInteger &left, const BigInteger &right);
+    friend BigInteger operator/(const BigInteger &dividend, const BigInteger &divisor);
+    friend BigInteger operator%(const BigInteger &left, const BigInteger &right);
 
-  friend BigInteger operator+ (const BigInteger& l, const BigInteger& r);
-  friend BigInteger operator- (const BigInteger& l, const BigInteger& r);
-  friend BigInteger operator* (const BigInteger& left, const BigInteger& right);
-  friend BigInteger operator/ (const BigInteger& dividend, const BigInteger& divisor);
-  friend BigInteger operator% (const BigInteger& left, const BigInteger& right);
+    BigInteger &operator+=(const BigInteger &num);
+    BigInteger &operator-=(const BigInteger &num);
+    BigInteger &operator*=(const BigInteger &num);
+    BigInteger &operator/=(const BigInteger &num);
+    BigInteger &operator%=(const BigInteger &num);
 
-  BigInteger& operator+= (const BigInteger& num);
-  BigInteger& operator-= (const BigInteger& num);
-  BigInteger& operator*= (const BigInteger& num);
-  BigInteger& operator/= (const BigInteger& num);
-  BigInteger& operator%= (const BigInteger& num);
-
-  static void extend(std::vector<int>&a, int length);
-  static std::vector<int> NaiveMultiplication(std::vector<int>& left, std::vector<int>& right);
-  static std::vector<int> KaratsubaMultiplication(std::vector<int>&left, std::vector<int>& y);
+    static void extend(std::vector<int> &a, int length);
+    static std::vector<int> NaiveMultiplication(std::vector<int> &left, std::vector<int> &right);
+    static std::vector<int> KaratsubaMultiplication(std::vector<int> &left, std::vector<int> &y);
 
 private:
-  int length;
-  bool positive;
-  std::vector<int> number;
+    int length;
+    bool positive;
+    std::vector<int> number;
 };
 
 #endif
