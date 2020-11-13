ad2bfcd336f8314f4a2ca7069e9491e39460797b
VisterK <vister.k30@gmail.com>
2020-11-13 20:34:24 +0300

    implemented list

--- module-1/homework/List/list.h
+++ module-1/homework/List/list.h
@@ -1,51 +1,77 @@
 #pragma once
+
 #include <cstddef>
 
+#ifndef LIST_H
+#define LIST_H
 
 namespace task {
 
+    class list {
 
-class list {
+    public:
 
-public:
+        list();
+        list(size_t count, const int &value = int());
+        list(const list& other);
 
-    list();
-    list(size_t count, const int& value = int());
-
-    ~list();
-    list& operator=(const list& other);
-
-
-    int& front();
-    const int& front() const;
-
-    int& back();
-    const int& back() const;
+        ~list();
+        list &operator=(const list &other);
+        bool operator==(const list &other);
 
 
-    bool empty() const;
-    size_t size() const;
-    void clear();
+        int &front();
+        const int &front() const;
+
+        int &back();
+        const int &back() const;
 
 
-    void push_back(const int& value);
-    void pop_back();
-    void push_front(const int& value);
-    void pop_front();
-    void resize(size_t count);
-    void swap(list& other);
+        bool empty() const;
+        size_t size() const;
+        void clear();
 
 
-    void remove(const int& value);
-    void unique();
-    void sort();
+        void push_back(const int &value);
+        void pop_back();
+        void push_front(const int &value);
+        void pop_front();
+        void resize(size_t count);
+        void swap(list &other);
 
-    // Your code goes here?..
 
-private:
+        void remove(const int &value);
+        void unique();
+        void sort();
 
-    // Your code goes here...
 
-};
+    private:
+        class Node;
+
+        static Node *ListSort(Node *head_, Node *tail);
+
+        class Node {
+        public:
+            Node *pNext;
+            Node *pPrev;
+            int data;
+
+            Node(int data = int(), Node *pNext = nullptr, Node *pPrev = nullptr) {
+                this->data = data;
+                this->pNext = pNext;
+                this->pPrev = pPrev;
+            }
+        };
+
+        int size_;
+        Node *head;
+        Node *tail;
+        void QuickSort(Node*, Node*);
+        Node* Partition(Node*, Node*);
+
+
+    };
 
 }  // namespace task
+
+#endif // LIST_H
