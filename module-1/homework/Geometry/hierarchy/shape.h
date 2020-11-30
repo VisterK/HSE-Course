45861332ef157c061163dfad150cbfa16272ba51
VisterK <vister.k30@gmail.com>
2020-11-30 17:09:14 +0300

    improved code quality

--- module-1/homework/Geometry/hierarchy/shape.h
+++ module-1/homework/Geometry/hierarchy/shape.h
@@ -11,10 +11,12 @@ public:
 
     virtual double perimeter() const = 0;
     virtual double area() const = 0;
+
     virtual bool operator==(const Shape& another) const = 0;
     virtual bool isCongruentTo(const Shape& another) const = 0;
     virtual bool isSimilarTo(const Shape& another) const = 0;
     virtual bool containsPoint(Point point) const = 0;
+
     virtual void rotate(Point center, double angle) = 0;
     virtual void reflex(Point center) = 0;
     virtual void reflex(Line axis) = 0;
